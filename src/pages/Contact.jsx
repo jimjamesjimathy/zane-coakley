9a0481818f6e170f54aa2ccf874c04f2fa931354
jimjamesjimathy <jamesalanmikal@gmail.com>
2022-12-10 14:54:10 -0500

    contact work

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,29 +1,31 @@
-import React from "react";
+import contact from "../assets/about.jpg";
 
 const Contact = () => {
   return (
     <section id="contact" className="h-screen pt-20">
       <div className="flex flex-wrap items-center h-full">
         <div className="w-full text-center">
-          <h2 className="font-amatic text-7xl">Contact me to shred the sicky</h2>
+          <h2 className="font-amatic text-7xl">
+            Contact me to shred the sicky
+          </h2>
         </div>
 
         {/* LEFT SIDE WITH TEXT AND IMAGE  */}
-        <div className="basis-2/5 h-2/3">
+        <div className="basis-2/5 h-2/3 bg-opaque-black border-r-2 flex flex-col items-center justify-evenly">
           <div className="">
-            <h3>heading</h3>
+            <h3 className="text-5xl">heading</h3>
           </div>
           <div className="">
-            <p>
+            <img src={contact} alt="" />
+          </div>
+          <div className="">
+            <p className="text-xl text-center">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sed
               fugit facere vero quisquam omnis vel.
             </p>
           </div>
-          <div className="">
-            <img src="" alt="" />
-          </div>
         </div>
-        
+
         {/* RIGHT SIDE WITH FORM  */}
         <div className="flex items-center basis-3/5 h-2/3 bg-opaque-black">
           <form
@@ -34,12 +36,12 @@ const Contact = () => {
             <div className="flex w-full gap-8 6">
               <input
                 type="text"
-                className="w-1/2 border-2"
+                className="p-2 w-1/2 border-2"
                 placeholder="First Name"
               />
               <input
                 type="text"
-                className="w-1/2 border-2"
+                className="p-2 w-1/2 border-2"
                 placeholder="Last Name"
               />
             </div>
@@ -48,31 +50,31 @@ const Contact = () => {
             <div className="flex w-full gap-8 6">
               <input
                 type="text"
-                className="w-1/2 border-2"
+                className="p-2 w-1/2 border-2"
                 placeholder="Phone Number (optional)"
               />
               <input
                 type="text"
-                className="w-1/2 border-2"
+                className="p-2 w-1/2 border-2"
                 placeholder="Email"
               />
             </div>
 
             {/* SUBJECT AND MESSAGE  */}
             <div className="flex flex-col items-center w-full gap-y-4">
-            <input
-              type="text"
-              className="w-full border-2"
-              placeholder="Subject"
+              <input
+                type="text"
+                className="p-2 w-full border-2"
+                placeholder="Subject"
               />
-            <textarea
-              rows={7}
-              type="text"
-              className="w-full border-2"
-              placeholder="What can i do for you?"
+              <textarea
+                rows={7}
+                type="text"
+                className="w-full border-2"
+                placeholder="What can i do for you?"
               />
-              </div>
-            <button className="w-2/3">Fire Away!</button>
+            </div>
+            <button className="px-4 py-2 rounded-md bg-green">Fire Away!</button>
           </form>
         </div>
       </div>
